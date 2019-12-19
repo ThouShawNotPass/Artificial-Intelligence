48535a12efbe50f8da6132cd81de311cba92170b
Justin Shaw <realjustinshaw@gmail.com>
2019-12-18 19:02:37 -0800

    updated

---
+++ MNIST.py
@@ -0,0 +1,2 @@
+# Learning the basics of neural networks with applied examples
+# Based off of this Medium tutorial: https://medium.com/tebs-lab/how-to-classify-mnist-digits-with-different-neural-network-architectures-39c75a0f03e3
